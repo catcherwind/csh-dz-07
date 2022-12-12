5c07c75f43d162857e1868f851688d0ae36bbbcf
Ksusha <ksushalive@yandex.ru>
2022-12-12 20:27:15 +0300

    init: solution of task 47

---
+++ z47/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = FillArray(rows, columns);
+
+PrintArray(array);
+
+double[,] FillArray(int arrayRows, int arrayColumns)
+{
+    double[,] filledArray = new double[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = Math.Round(random.NextDouble(), 2);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(double[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
