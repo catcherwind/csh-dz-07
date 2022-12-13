4cd87621842fc83937cd355a684fe43ba4886fd2
Ksusha <ksushalive@yandex.ru>
2022-12-13 19:38:35 +0300

    init: solution of task 52

---
+++ z52/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillArray(int arrayRows, int arrayColumns)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(-10, 10);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] SearchArithmeticMeanArray(int[,] array)
+{
+    double[] amArray = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           amArray[j] += array[i,j]; 
+        }
+    }
+    for (int i = 0; i < amArray.Length; i++){
+        amArray[i] = amArray[i] / amArray.Length;
+    }
+    return amArray;
+}
+
+int[,] array = FillArray(rows, columns);
+PrintArray(array);
+double[] amArray = SearchArithmeticMeanArray(array);
+Console.WriteLine();
+Console.WriteLine(String.Join(' ', amArray));
